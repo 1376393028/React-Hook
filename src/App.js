48a66887e699936600a0325563181a2535532d22
zhazhahui <1376393028@qq.com>
2021-02-13 21:39:15 +0800

    react-hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+ let memoizedState = []; 
+ let cursor = 0; 
+
+ function render() {
+  cursor = 0;
+  ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+  );
+ }
+ 
+ function useState(initialValue) {
+   memoizedState[cursor] = memoizedState[cursor] || initialValue;
+   const currentCursor = cursor;
+   function setState(newState) {
+     memoizedState[currentCursor] = newState;
+     render();
+   }
+   return [memoizedState[cursor++], setState]; 
+ }
+ 
+ function useEffect(callback, depArray) {
+   const hasNoDeps = !depArray;
+   const deps = memoizedState[cursor];
+   const hasChangedDeps = deps
+     ? !depArray.every((el, i) => el === deps[i]): true;
+   if (hasNoDeps || hasChangedDeps) {
+     callback();
+     memoizedState[cursor] = depArray;
+   }
+   cursor++;
+ }
+
 
 function App() {
+  const [count,setCount] = useState(0);
+  const [age,steAge] = useState(0);
+  useEffect(() => {
+    console.log(`click a ${count}+${age}time`)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}> Click me {count}</button>
+      <p>You clicked {age} times</p>
+      <button onClick={() => steAge(age + 1)}> Click me {age}</button>
     </div>
-  );
+ );
 }
-
 export default App;
